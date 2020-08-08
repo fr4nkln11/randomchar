e7bb64edecce8eeaf8843fb00461c8a491798bc8
Franklin Ikeh <69267493+fr4nkl1n-1k3h@users.noreply.github.com>
2020-08-08 18:21:55 +0100

    Update randomChar.py

--- randomChar/randomChar.py
+++ randomChar/randomChar.py
@@ -1,1 +1,41 @@
+import random
+import string
+import secrets
 
+
+def letter():
+    return random.choice(string.ascii_letters)
+
+
+def loCase():
+    return random.choice(string.ascii_lowercase)
+
+
+def upCase():
+    return random.choice(string.ascii_uppercase)
+
+
+def digit():
+    return random.choice(string.digits)
+
+
+def symbol():
+    return random.choice(string.punctuation)
+
+
+def all():
+    randomCharList = [letter(), digit(), symbol()]
+    return random.choice(randomCharList)
+    
+def password(length):
+	charList = []
+	
+	for i in range(length):
+	   charList.append(secrets.choice(string.ascii_letters + string.digits + string.punctuation))
+	   
+	random.shuffle(charList)
+	return ''.join(charList)
+    
+if __name__ == "__main__":
+    print("\nThis module was created by fr4nkl1n-1k3h\n")
+    print(f"password(9): {password(9)}")
