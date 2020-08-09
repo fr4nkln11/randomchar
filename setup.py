eddd735662890c1b2e02ccc9cc648959eaa2a1ca
Franklin Ikeh <69267493+fr4nkl1n-1k3h@users.noreply.github.com>
2020-08-09 12:45:58 +0100

    Add files via upload
    Created setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="randomChar-YOUR-USERNAME-HERE", # Replace with your own username
+    version="0.0.1",
+    author="Franklin Ikeh",
+    author_email="ikehfranklind3c0d3r@gmail.com",
+    description="A package that makes random character or sequence generation much easier",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fr4nkl1n-1k3h/randomChar",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
